39eea8414c0c28a69b4982b17656baa27848cfcf
Nikola Bodrozic <you@example.com>
2017-06-20 22:57:05 +0200

    render select single, select multiple, radio & checkox from database

--- src/AppBundle/Form/SobaType.php
+++ src/AppBundle/Form/SobaType.php
@@ -5,10 +5,13 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class SobaType extends AbstractType
 {
     /**
+	 * radio, checkbox and select elements rendered from database
+	 * 
      * @param FormBuilderInterface $builder
      * @param array $options
      */
@@ -16,8 +19,30 @@ class SobaType extends AbstractType
     {
         $builder
             ->add('imesobe')
-            ->add('zgrada')
-        ;
+            ->add('zgrada', EntityType::class, array(
+				// query choices from this entity
+				'class' => 'AppBundle:Zgrada',
+
+				// use the User.username property as the visible option string
+				'choice_label' => 'ime',
+// select single
+//				'multiple' => false,
+//				'expanded' => false 	
+
+// select multiple
+//				'multiple' => true,
+//				'expanded' => false 	
+
+// checkbox checkboxes
+//				'multiple' => true,
+//				'expanded' => true 	
+
+// radio buttons
+				'multiple' => false,
+				'expanded' => true 		
+				)
+			)
+			;
     }
     
     /**
