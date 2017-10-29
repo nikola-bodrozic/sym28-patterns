c866742bb99c1d01de76861d9707960a864d9cc9
Nikola Bodrozic <nikola.bodrozic@example.com>
2017-10-29 15:49:38 -0400

    fixed readme and updated database.sql

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 Symfony patterns
 ================
+##Deploy on test environment
+- `composer install`
+- import data from database.sql `mysql -u root -p test < database.sql`
+- `php app/console server:run`
+- `http://localhost:8000`
+- Test `php phpunit-5.7.phar -c app/`
 
-- one to many relatioship in zgrada_index route
-- select & checkox in chkbox_index
-- symfony service
+##Features
+- one to many relatioship in `zgrada_index`
+- checkox || radio buttons in `chkbox_index`
+- symfony service container and dependacy injection in `sym_service`
--- database.sql
+++ database.sql
@@ -1,81 +1,168 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               5.5.5-10.1.19-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win32
--- HeidiSQL Version:             8.3.0.4694
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 29, 2017 at 03:37 PM
+-- Server version: 5.7.19-0ubuntu0.17.04.1
+-- PHP Version: 7.0.22-0ubuntu0.17.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Dumping structure for table uzorsym28.tdk_chkbox
-CREATE TABLE IF NOT EXISTS `tdk_chkbox` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+--
+-- Database: `sym28patt`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tdk_chkbox`
+--
+
+CREATE TABLE `tdk_chkbox` (
+  `id` int(11) NOT NULL,
   `ime` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `prvi` tinyint(1) NOT NULL,
   `drugi` tinyint(1) NOT NULL,
-  `isAttending` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `isAttending` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Dumping data for table uzorsym28.tdk_chkbox: ~0 rows (approximately)
-/*!40000 ALTER TABLE `tdk_chkbox` DISABLE KEYS */;
+--
+-- Dumping data for table `tdk_chkbox`
+--
+
 INSERT INTO `tdk_chkbox` (`id`, `ime`, `prvi`, `drugi`, `isAttending`) VALUES
-	(1, 'pera', 1, 0, 'true');
-/*!40000 ALTER TABLE `tdk_chkbox` ENABLE KEYS */;
+(1, 'dinner at mikes\'s', 1, 0, 'yes');
 
+-- --------------------------------------------------------
 
--- Dumping structure for table uzorsym28.tdk_soba
-CREATE TABLE IF NOT EXISTS `tdk_soba` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+--
+-- Table structure for table `tdk_soba`
+--
+
+CREATE TABLE `tdk_soba` (
+  `id` int(11) NOT NULL,
   `zgrada_id` int(11) DEFAULT NULL,
-  `imesobe` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `IDX_BDCE54F297DDB473` (`zgrada_id`),
-  CONSTRAINT `FK_BDCE54F297DDB473` FOREIGN KEY (`zgrada_id`) REFERENCES `tdk_zgrada` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `imesobe` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Dumping data for table uzorsym28.tdk_soba: ~0 rows (approximately)
-/*!40000 ALTER TABLE `tdk_soba` DISABLE KEYS */;
+--
+-- Dumping data for table `tdk_soba`
+--
+
 INSERT INTO `tdk_soba` (`id`, `zgrada_id`, `imesobe`) VALUES
-	(1, 2, 'tertert');
-/*!40000 ALTER TABLE `tdk_soba` ENABLE KEYS */;
+(1, 1, 'royal room');
 
+-- --------------------------------------------------------
 
--- Dumping structure for table uzorsym28.tdk_task
-CREATE TABLE IF NOT EXISTS `tdk_task` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+--
+-- Table structure for table `tdk_task`
+--
+
+CREATE TABLE `tdk_task` (
+  `id` int(11) NOT NULL,
   `task` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `dueDate` datetime NOT NULL,
-  `dateUpdated` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `dateUpdated` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Dumping data for table uzorsym28.tdk_task: ~4 rows (approximately)
-/*!40000 ALTER TABLE `tdk_task` DISABLE KEYS */;
+--
+-- Dumping data for table `tdk_task`
+--
+
 INSERT INTO `tdk_task` (`id`, `task`, `dueDate`, `dateUpdated`) VALUES
-	(1, 'pravi', '2013-01-01 00:04:00', '0000-00-00 00:00:00'),
-	(2, 'jos jedan', '2018-01-01 00:00:00', '0000-00-00 00:00:00'),
-	(3, 'jos jedan', '2018-01-01 00:00:00', '0000-00-00 00:00:00'),
-	(4, 'prvi', '2011-01-01 00:00:00', '0000-00-00 00:00:00');
-/*!40000 ALTER TABLE `tdk_task` ENABLE KEYS */;
+(1, 'pickup food', '2012-01-01 00:00:00', '2017-10-29 15:09:25'),
+(2, 'pickup food', '2012-01-01 00:00:00', '2017-10-29 15:36:38');
 
+-- --------------------------------------------------------
 
--- Dumping structure for table uzorsym28.tdk_zgrada
-CREATE TABLE IF NOT EXISTS `tdk_zgrada` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ime` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+--
+-- Table structure for table `tdk_zgrada`
+--
 
--- Dumping data for table uzorsym28.tdk_zgrada: ~2 rows (approximately)
-/*!40000 ALTER TABLE `tdk_zgrada` DISABLE KEYS */;
+CREATE TABLE `tdk_zgrada` (
+  `id` int(11) NOT NULL,
+  `ime` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `tdk_zgrada`
+--
+
 INSERT INTO `tdk_zgrada` (`id`, `ime`) VALUES
-	(1, 'petronas'),
-	(2, 'usce');
-/*!40000 ALTER TABLE `tdk_zgrada` ENABLE KEYS */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+(1, 'petronas'),
+(2, 'empire state building');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tdk_chkbox`
+--
+ALTER TABLE `tdk_chkbox`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tdk_soba`
+--
+ALTER TABLE `tdk_soba`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_BDCE54F297DDB473` (`zgrada_id`);
+
+--
+-- Indexes for table `tdk_task`
+--
+ALTER TABLE `tdk_task`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tdk_zgrada`
+--
+ALTER TABLE `tdk_zgrada`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tdk_chkbox`
+--
+ALTER TABLE `tdk_chkbox`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `tdk_soba`
+--
+ALTER TABLE `tdk_soba`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `tdk_task`
+--
+ALTER TABLE `tdk_task`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `tdk_zgrada`
+--
+ALTER TABLE `tdk_zgrada`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tdk_soba`
+--
+ALTER TABLE `tdk_soba`
+  ADD CONSTRAINT `FK_BDCE54F297DDB473` FOREIGN KEY (`zgrada_id`) REFERENCES `tdk_zgrada` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
